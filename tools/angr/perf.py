72611e26c1b4b6c530524d7a0691da55ce33ddff
Carter Yagemann <yagemann@gatech.edu>
2021-09-23 13:52:07 -0400

    Add decoder for turning Linux Perf PT traces into basic block sequences.

---
+++ tools/angr/perf.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+#
+# Copyright 2021 Carter Yagemann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import gzip
+import os
+import re
+import shutil
+import subprocess
+import sys
+import tempfile
+
+BRANCH_MNEMONICS = {
+    'jb', 'jbe', 'jl', 'jle', 'jmp', 'jmpq', 'jnb', 'jnbe', 'jnl', 'jnle',
+    'jns', 'jnz', 'jo', 'jp', 'js', 'jz', 'bnd jmp', 'loop', 'jno', 'jnp',
+    'jnae', 'jc', 'jae', 'jnc', 'jna', 'ja', 'jnge', 'jge', 'jng', 'jg', 'jpe',
+    'jpo', 'jcxz', 'jecxz'
+}
+
+CALL_MNEMONICS = {
+    'callq', 'bnd callq'
+}
+
+RET_MNEMONICS = {
+    'retq', 'bnd retq'
+}
+
+SYSCALL_MNEMONICS = {
+    'syscall', 'sysenter'
+}
+
+# change of flow
+COF_MNEMONICS = (BRANCH_MNEMONICS | CALL_MNEMONICS
+        | SYSCALL_MNEMONICS | RET_MNEMONICS)
+
+PTXED = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../pt/build/bin/ptxed')
+IPT_SCRIPTS = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../pt/libipt/script')
+AUX_REGEX = re.compile('perf.data-aux-idx(?P<idx>[0-9]+).bin')
+
+class DisasmError(Exception):
+    pass
+
+class Disasm(object):
+
+    CONTEXT_REGEX = re.compile("\[context: pid-(?P<pid>[0-9a-f]+)\]")
+    ADDR_REGEX = re.compile("(?P<time>[0-9a-f]+)  (?P<addr>[0-9a-f]+)  (?P<mnemonic>[a-z ]+)")
+
+    def __init__(self, perf_fp, aux_fp, sideband_fp):
+        """A class for managing ptxed as it disassembles a perf PT trace.
+
+        Keyword Arguments:
+        perf_fp -- Filepath to perf.data file.
+        aux_fp -- Filepath to Perf aux file.
+        sideband_fp -- Filepath to Perf sideband file.
+
+        Raises:
+        AssertError if filepaths do not exist.
+        subprocess.CalledProcessError if opts cannot be extracted from sideband.
+        """
+        assert(os.path.isfile(perf_fp))
+        assert(os.path.isfile(aux_fp))
+        assert(os.path.isfile(sideband_fp))
+
+        # libipt getopts script
+        ipt_getopts = os.path.join(IPT_SCRIPTS, 'perf-get-opts.bash')
+        assert(os.path.isfile(ipt_getopts))
+
+        self.perf = perf_fp
+        self.aux = aux_fp
+        self.sideband = sideband_fp
+
+        # get opts
+        self.opts = subprocess.run(['/bin/bash', ipt_getopts, '-m', os.path.basename(self.sideband)],
+                capture_output=True, check=True).stdout.decode('ascii').split(' ')[1:]
+
+        # start disassembly
+        cmd = [PTXED, '--att', '--time', '--sb:switch'] + self.opts + ['--pevent:vdso-x64', '--event:tick', '--pt', self.aux]
+        self.ptxed = subprocess.Popen(cmd, bufsize=-1, stdout=subprocess.PIPE, universal_newlines=True)
+
+        self.stdout = self.ptxed.stdout
+        self.next_event()
+
+    def next_event(self):
+        new_block = False
+        while True:
+            line = self.stdout.readline()
+
+            if len(line) < 1:
+                # disassembly is done
+                self.pid = None
+                self.time = None
+                self.addr = None
+                return
+
+            res = self.CONTEXT_REGEX.match(line)
+            if res:
+                self.pid = int(res.group('pid'), 16)
+                continue
+
+            res = self.ADDR_REGEX.match(line)
+            if res:
+                self.time = int(res.group('time'), 16)
+                self.addr = int(res.group('addr'), 16)
+                if new_block:
+                    return
+
+                mnemonic = res.group('mnemonic').rstrip()
+                if mnemonic in COF_MNEMONICS:
+                    # this instruction ends the current basic block,
+                    # next instruction is the start of a new block
+                    new_block = True
+
+def disasm_perf(perf_fp, output_fp):
+    """Disassemble a perf.data file into per-thread basic block sequences.
+
+    Keyword Arguments:
+    perf_fp -- Filepath to the perf.data file.
+    output_fp -- Filepath to write the decoded trace into.
+
+    Raises:
+    AssertionError if necessary scripts, programs, inputs, or outputs are missing or invalid.
+    DisasmError if disassembly fails.
+    """
+    assert(os.path.isfile(perf_fp))
+    assert(not os.path.exists(output_fp))
+
+    # libipt aux extraction script
+    ipt_aux = os.path.join(IPT_SCRIPTS, 'perf-read-aux.bash')
+    assert(os.path.isfile(ipt_aux))
+
+    # libipt sideband extraction script
+    ipt_sideband = os.path.join(IPT_SCRIPTS, 'perf-read-sideband.bash')
+    assert(os.path.isfile(ipt_sideband))
+
+    temp = tempfile.mkdtemp(prefix='aperf-')
+    os.chdir(temp)
+
+    # we need to symlink perf.data into the temp directory because some of the
+    # libipt scripts don't work correctly if perf.data isn't in the CWD
+    os.symlink(perf_fp, 'perf.data')
+
+    # Setup for ptxed
+    #   1) extract per-core perf aux data (the PT trace data)
+    ret = subprocess.run(['/bin/bash', ipt_aux])
+    if ret.returncode != 0:
+        shutil.rmtree(temp)
+        raise DisasmError("Failed to extract aux data")
+    #   2) extract per-core sideband data
+    ret = subprocess.run(['/bin/bash', ipt_sideband])
+    if ret.returncode != 0:
+        shutil.rmtree(temp)
+        raise DisasmError("Failed to extract sideband data")
+
+    # disassemble each core's trace
+    workers = list()
+    for entry in os.listdir('.'):
+        res = AUX_REGEX.match(entry)
+        if res:
+            idx = res.group('idx')
+            sideband = 'perf.data-sideband-cpu%s.pevent' % idx
+            if os.path.isfile(sideband):
+                workers.append(Disasm('perf.data', entry, sideband))
+
+    with gzip.open(output_fp, 'wt') as ofile:
+        curr_pid = None
+        while True:
+            # prune finished workers
+            done = [w for w in workers if w.time is None]
+            for w in done:
+                workers.remove(w)
+            if len(workers) < 1:
+                break
+
+            # determine which core has the next event
+            timestamps = sorted(list(enumerate([w.time for w in workers])), key=lambda w: w[1])
+            # record next event
+            next_worker = workers[timestamps[0][0]]
+            if next_worker.pid != curr_pid:
+                # we've switched PIDs
+                ofile.write("[pid: %d]\n" % next_worker.pid)
+                curr_pid = next_worker.pid
+            ofile.write("%x\n" % next_worker.addr)
+            # advance worker
+            next_worker.next_event()
+
+    # cleanup
+    shutil.rmtree(temp)
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        print("Usage: perf.py <perf.data> <output_file>")
+        sys.exit(1)
+
+    disasm_perf(sys.argv[1], sys.argv[2])
