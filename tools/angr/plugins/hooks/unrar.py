d9cf2b6b566ed4f5085e8184d57748c581fd9d9a
Carter Yagemann <yagemann.1@osu.edu>
2022-10-14 16:02:00 -0400

    Use simprocs to abstract away unrar's RawReader and nullify logging functions since we don't care about these for ARCUS' analysis.

--- tools/angr/plugins/hooks/unrar.py
+++ tools/angr/plugins/hooks/unrar.py
@@ -18,6 +18,9 @@
 import logging
 import angr
 
+from .libc import libc_mbsrtowcs
+from .libc import WCHAR_BYTES
+
 log = logging.getLogger(name=__name__)
 
 
@@ -47,11 +50,129 @@ class unrar__Z10Checksum14tPKvm(angr.SimProcedure):
         # building constraints for checksums is hard, so just under-constrain
         return self.state.solver.BVS("checksum14", self.state.arch.bits)
 
+class unrar__Z9UtfToWidePKcPwm(angr.SimProcedure):
+
+    def run(self, src, dest, dest_size):
+        src_ptr = self.inline_call(angr.SIM_PROCEDURES["libc"]["malloc"], self.state.arch.bytes).ret_expr
+        self.state.memory.store(src_ptr, src, endness=self.state.arch.memory_endness)
+        self.inline_call(libc_mbsrtowcs, dest, src_ptr, dest_size // WCHAR_BYTES, 0)
+        self.inline_call(angr.SIM_PROCEDURES["libc"]["free"], src_ptr)
+        return
+
+class unrar__Z3LogPKwS0_z(angr.SimProcedure):
+
+    def run(self, arc_name, fmt):
+        # don't care about logging
+        return
+
+class unrar__ZN7Archive15BrokenHeaderMsgEv(angr.SimProcedure):
+
+    def run(self):
+        # don't care about logging
+        return
+
+class unrar__ZN12ErrorHandler14CreateErrorMsgEPKwS1(angr.SimProcedure):
+
+    def run(self, arc_name, file_name):
+        # don't care about logging
+        return
+
+## RawRead class
+
+class unrar__ZN7RawRead4Get1Ev(angr.SimProcedure):
+
+    def run(self):
+        # returns a byte read from input file
+        val = self.state.solver.BVS("raw_read_get1", 8)
+        if self.state.arch.bits > val.length:
+            val = val.zero_extend(self.state.arch.bits - val.length)
+        return val
+
+class unrar__ZN7RawRead4Get2Ev(angr.SimProcedure):
+
+    def run(self):
+        # returns a ushort read from input file
+        val = self.state.solver.BVS("raw_read_get2", 16)
+        if self.state.arch.bits > val.length:
+            val = val.zero_extend(self.state.arch.bits - val.length)
+        return val
+
+class unrar__ZN7RawRead4Get4Ev(angr.SimProcedure):
+
+    def run(self):
+        # returns a uint read from input file
+        val = self.state.solver.BVS("raw_read_get4", 32)
+        if self.state.arch.bits > val.length:
+            val = val.zero_extend(self.state.arch.bits - val.length)
+        return val
+
+class unrar__ZN7RawRead4GetVEv(angr.SimProcedure):
+
+    def run(self):
+        # returns a uint64 read from input file
+        val = self.state.solver.BVS("raw_read_getv", 64)
+        if self.state.arch.bits > val.length:
+            val = val.zero_extend(self.state.arch.bits - val.length)
+        return val
+
+class unrar__ZN7RawRead6SetPosEm(angr.SimProcedure):
+
+    def run(self, pos):
+        # simulating RawRead, so don't actually need to move position
+        return
+
+class unrar__ZN7RawRead5ResetEv(angr.SimProcedure):
+
+    def run(self):
+        # simulating RawRead, so don't actually need to reset
+        return
+
+class unrar__ZN7RawRead4ReadEm(angr.SimProcedure):
+
+    def run(self, size):
+        # simulating RawRead, so don't actually need to read from file
+        return
+
+class unrar__ZN7RawRead4ReadEPhm(angr.SimProcedure):
+
+    def run(self, src_data, size):
+        # simulating RawRead, so don't actually need to read from file
+        return
+
+class unrar__Z9cleandataPvm(angr.SimProcedure):
+
+    def run(self, data, size):
+        # unrar uses this method to wipe data from memory, but since
+        # we're not looking for leaks, we can just skip this to save
+        # a ton of steps
+        return
+
 unrar_hooks = {
     "_Z9InitCRC32Pj": unrar__Z9InitCRC32Pj,
     "_Z5CRC32jPKvm": unrar__Z5CRC32jPKvm,
     "_ZL10InitTablesv": unrar__ZL10InitTablesv,
     "_Z10Checksum14tPKvm": unrar__Z10Checksum14tPKvm,
+    "_Z9UtfToWidePKcPwm": unrar__Z9UtfToWidePKcPwm,
+    "_Z3LogPKwS0_z": unrar__Z3LogPKwS0_z,
+    "_ZN7Archive15BrokenHeaderMsgEv": unrar__ZN7Archive15BrokenHeaderMsgEv,
+    "_ZN12ErrorHandler14CreateErrorMsgEPKwS1_": unrar__ZN12ErrorHandler14CreateErrorMsgEPKwS1,
+    "_ZN7RawRead4Get4Ev": unrar__ZN7RawRead4Get4Ev,
+    "_ZN7RawRead6SetPosEm": unrar__ZN7RawRead6SetPosEm,
+    "_ZN7RawRead4Get1Ev": unrar__ZN7RawRead4Get1Ev,
+    "_ZN7RawRead4GetVEv": unrar__ZN7RawRead4GetVEv,
+    # returns same type as getv
+    "_ZN7RawRead4Get8Ev": unrar__ZN7RawRead4GetVEv,
+    # returns same type as get4
+    "_ZN7RawRead8GetCRC50Ev": unrar__ZN7RawRead4Get4Ev,
+    "_ZN7RawRead8GetCRC15Eb": unrar__ZN7RawRead4Get4Ev,
+    "_ZN7RawRead8GetVSizeEm": unrar__ZN7RawRead4Get4Ev,
+    # TODO: _ZN7RawRead4GetWEPwm (RawRead::GetW(wchar_t*, unsigned long))
+    # TODO: _ZN7RawRead4GetBEPvm (RawRead::GetB(void*, unsigned long))
+    "_ZN7RawRead5ResetEv": unrar__ZN7RawRead5ResetEv,
+    "_ZN7RawRead4ReadEm": unrar__ZN7RawRead4ReadEm,
+    "_ZN7RawRead4Get2Ev": unrar__ZN7RawRead4Get2Ev,
+    "_ZN7RawRead4ReadEPhm": unrar__ZN7RawRead4ReadEPhm,
+    "_Z9cleandataPvm": unrar__Z9cleandataPvm,
 }
 
 hook_condition = ("unrar", unrar_hooks)
